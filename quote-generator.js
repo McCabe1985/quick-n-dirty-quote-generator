39f475cae6385b38499b58ccef44efd0f1159b77
Patrick <78211593+McCabe1985@users.noreply.github.com>
2022-02-17 20:11:14 +0000

    Update quote-generator.js

--- quote-generator.js
+++ quote-generator.js
@@ -3,7 +3,8 @@ const quote = document.querySelector(".quote")
 
 const quotes = ['"Be yourself; everyone else is already taken" - Oscar Wilde', 
 '“Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe" - Albert Einstein',
-'“So many books, so little time.” - Frank Zappa', '“A room without books is like a body without a soul.” - Marcus Tullius Cicero', 
+'“So many books, so little time.” - Frank Zappa', 
+'“A room without books is like a body without a soul.” - Marcus Tullius Cicero', 
 '“If you want to know what a man\'s like, take a good look at how he treats his inferiors, not his equals.” - J.K Rowling',
 '“If you tell the truth, you don\'t have to remember anything.” - Mark Twain',
 '“Live as if you were to die tomorrow. Learn as if you were to live forever.” - Mahatma Ghandi',
